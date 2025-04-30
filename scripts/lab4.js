47080d2ff511b8711779140b147b56c11a50f65c
Ali Mortada Alsadah <130116608+aalsadah@users.noreply.github.com>
2025-04-29 19:23:27 -0700

    Update lab4.js
    fixed bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -8,13 +8,12 @@
 function sumValues(num1, num2, add) {
     if (add) {
         let result = 0;
-
         result = num1 + num2;
 
         return result;
     }
     else {
-        return add;
+        return false;
     }
 }
 
@@ -25,11 +24,12 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if(prices.length===0)return false;
     let discounted = []
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
